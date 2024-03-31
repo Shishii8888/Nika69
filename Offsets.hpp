09344d85090d86776caec162f66b577d1e9f690f
Shishii8888 <165109158+Shishii8888@users.noreply.github.com>
2024-03-31 01:31:02 -0300

    Update Offsets.hpp

--- Offsets.hpp
+++ Offsets.hpp
@@ -9,19 +9,19 @@ constexpr long OFF_ENTITY_LIST = 0x1DD40F8; //[Miscellaneous]->cl_entitylist
 constexpr long OFF_GLOBAL_VARS = 0x16f8b90; //[Miscellaneous]->GlobalVars
 
 // Buttons
-constexpr long OFF_IN_ATTACK = 0x073f2798; //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x073f2878; //[Buttons]->in_jump
-constexpr long OFF_IN_DUCK = 0x073f2978; //[Buttons]->in_duck
-constexpr long OFFSET_IN_FORWARD = 0x073f26c8; //[Buttons]->in_forward
+constexpr long OFF_IN_ATTACK = 0x073f2818; //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073f2908; //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x073f29f8; //[Buttons]->in_duck
+constexpr long OFFSET_IN_FORWARD = 0x073f2748; //[Buttons]->in_forward
 constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2af0; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
 constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2aec; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
-constexpr long OFFSET_TRAVERSAL_RELEASE_TIME = 0x2af8;		   //[RecvTable.DT_LocalPlayerExclusive]->m_traversalReleaseTime
+constexpr long OFFSET_TRAVERSAL_RELEASE_TIME = 0x2af8; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalReleaseTime
 constexpr long OFFSET_WALL_RUN_START_TIME = 0x3584; //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
 constexpr long OFFSET_WALL_RUN_CLEAR_TIME = 0x3588; //RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
 
 // Player
 constexpr long OFF_VIEW_MATRIX = 0x11a350; //[RecvTable.DT_Player]->ViewMatrix 
-constexpr long OFF_VIEW_RENDER = 0x73f14f8; //[RecvTable.DT_Player]->ViewRender
+constexpr long OFF_VIEW_RENDER = 0x73f1578; //[RecvTable.DT_Player]->ViewRender
 constexpr long OFF_VIEW_MODELS = 0x2d18; //[RecvTable.DT_Player]->m_hViewModels
 constexpr long OFF_ZOOMING = 0x1bd1; //[RecvTable.DT_Player]->m_bZooming
 constexpr long OFF_LOCAL_ORIGIN = 0x017c; //[DataMap.CBaseViewModel]->m_vecAbsOrigin
@@ -33,7 +33,7 @@ constexpr long OFF_NAME = 0x0471; //[RecvTable.DT_BaseEntity]->m_iName
 constexpr long OFF_SIGNIFIER_NAME = 0x0468; //[RecvTable.DT_BaseEntity]->m_iSignifierName
 constexpr long OFF_LIFE_STATE = 0x0680; //[RecvTable.DT_Player]->m_lifeState
 constexpr long OFF_BLEEDOUT_STATE = 0x26e0; //[RecvTable.DT_Player]->m_bleedoutState
-constexpr long OFF_LAST_VISIBLE_TIME = 0x1990 + 0x3; //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
+constexpr long OFF_LAST_VISIBLE_TIME = 0x198d + 0x3; //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
 constexpr long OFF_LAST_AIMEDAT_TIME = 0x198d + 0x3 + 0x8; //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
 constexpr long OFF_VIEW_ANGLES = 0x2534 - 0x14; //[DataMap.C_Player]->m_ammoPoolCapacity - 0x14
 constexpr long OFF_PUNCH_ANGLES = 0x2438; //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
@@ -41,10 +41,10 @@ constexpr long OFF_BREATH_ANGLES = (OFF_VIEW_ANGLES - 0x10);
 constexpr long OFF_STUDIOHDR = 0xff0; //[Miscellaneous]->CBaseAnimating!m_pStudioHdr
 constexpr long OFF_BONES = 0x0da0 + 0x48; //[RecvTable.DT_BaseAnimating]->m_nForceBone + 0x48
 constexpr long OFF_CAMERAORIGIN = 0x1ed0; //[Miscellaneous]->CPlayer!camera_origin
-constexpr long OFF_MODELNAME = 0x0390; //[DataMap.C_BaseEntity]->m_ModelName
+constexpr long OFF_MODELNAME = 0x0030; //[DataMap.C_BaseEntity]->m_ModelName
 constexpr long OFF_NAMELIST = 0xc5fec80; //[Miscellaneous]->NameList
-constexpr long OFF_YAW = 0x21f4 - 0x8; //[DataMap.C_Player]=>m_currentFramePlayer.m_ammoPoolCount - 0x8
-constexpr long OFF_NAMEINDEX = 0x38; //[RecvTable.DT_BaseEntity]
+constexpr long OFF_YAW = 0x223c - 0x8; //[DataMap.C_Player]=>m_currentFramePlayer.m_ammoPoolCount - 0x8
+constexpr long OFF_NAMEINDEX = 0x0578; //[RecvTable.DT_BaseEntity]
 constexpr long OFF_XPLEVEL = 0x3694;   //m_xp
 
 // Weapon
